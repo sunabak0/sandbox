0e7861555d6e245c528bfde997ee3b730a48c5b2
sunakan <sunakan03@gmail.com>
2022-12-31 22:06:04 +0900

    test: 2022-12-31T22:06:04

--- scripts/create-pr-for-submodule-updates.sh
+++ scripts/create-pr-for-submodule-updates.sh
@@ -16,24 +16,22 @@
 #
 
 
-readonly WORKING_DIR="$(git rev-parse --show-toplevel)"
 readonly SUBMODULE="akiyadego-openapi"
 readonly CURRENT_BRANCH="${CURRENT_BRANCH:-$(git branch --show-current)}"
-readonly CURRENT_SUBMODULE_COMMIT_ID="$(cat ${WORKING_DIR}/.git/modules/${SUBMODULE}/ORIG_HEAD)"
-readonly LATEST_SUBMODULE_COMMIT_ID="$(cat ${WORKING_DIR}/.git/modules/${SUBMODULE}/HEAD)"
 
-cd "${WORKING_DIR}/${SUBMODULE}"
+cd "${SUBMODULE}"
 git switch main
-pwd
-
-git submodule update --init --recursive
-
-#cd akiyadego-openapi && git switch main && git restore .
-#git submodule status akiyadego-openapi | tr -d ' ' | sed 's/^+//'
+git stage .
+git stash -q
+cd -
+current_module_commit_id="$(git submodule status | cut -d ' ' -f2)"
+readonly current_module_commit_id
+git submodule update --init --recursive --remote
+latest_module_commit_id="$(git submodule status | sed 's/^+/ /' | cut -d ' ' -f2)"
 
 echo '============='
 echo "${WORKING_DIR}"
 echo "${CURRENT_BRANCH}"
-echo "${CURRENT_SUBMODULE_COMMIT_ID}"
-echo "${LATEST_SUBMODULE_COMMIT_ID}"
+echo "${current_module_commit_id}"
+echo "${latest_module_commit_id}"
 echo '============='
