fdf694c78b0a6ca2f56d363c7f9a15d94ab5c3b4
sunakan <sunakan03@gmail.com>
2022-12-31 21:28:54 +0900

    test: 2022-12-31T21:28:54

--- scripts/create-pr-for-submodule-updates.sh
+++ scripts/create-pr-for-submodule-updates.sh
@@ -15,14 +15,20 @@
 # - To: main
 #
 
-readonly CURRENT_BRANCH=${CURRENT_BRANCH:-$(git branch --show-current)}
-echo "----"
-echo ${CURRENT_BRANCH}
-echo "----"
+git submodule update --init --recursive --remote
 
-git status
+readonly WORKING_DIR="$(git rev-parse --show-toplevel)"
+readonly SUBMODULE="akiyadego-openapi"
+readonly CURRENT_BRANCH="${CURRENT_BRANCH:-$(git branch --show-current)}"
+readonly CURRENT_SUBMODULE_COMMIT_ID="$(cat ${WORKING_DIR}/.git/modules/${SUBMODULE}/ORIG_HEAD)"
+readonly LATEST_SUBMODULE_COMMIT_ID="$(cat ${WORKING_DIR}/.git/modules/${SUBMODULE}/HEAD)"
 
-#git submodule update --init --recursive --remote
 #cd akiyadego-openapi && git switch main && git restore .
 #git submodule status akiyadego-openapi | tr -d ' ' | sed 's/^+//'
 
+echo '============='
+echo "${WORKING_DIR}"
+echo "${CURRENT_BRANCH}"
+echo "${CURRENT_SUBMODULE_COMMIT_ID}"
+echo "${LATEST_SUBMODULE_COMMIT_ID}"
+echo '============='
